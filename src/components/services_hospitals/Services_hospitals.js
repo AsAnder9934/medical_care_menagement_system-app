809e8521ab7f7de329abe55117a313d5643af58e
AsAnder9934 <149382112+AsAnder9934@users.noreply.github.com>
2024-05-27 11:25:36 +0200

    sevices_finish

--- src/components/services_hospitals/Services_hospitals.js
+++ src/components/services_hospitals/Services_hospitals.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./Services_patients.css";
 import logo_white from "./tmp/white_logo_1.png";
 import db_icon from "./tmp/data_base.png";
 import add_icon from "./tmp/add_icon.png";
@@ -7,18 +6,18 @@ import map_icon from "./tmp/map_icon.png";
 
 import { Link } from "react-router-dom";
 
-function Services_patients() {
+function Services_hospitals() {
   return (
     <div className="services">
       <div className="top">
-        <div className="service_title">Patients</div>
+        <div className="service_title">Hospitals</div>
         <img className="logo_white" src={logo_white} alt="logo_white"></img>
         <div className="services_line"></div>
       </div>
       <div className="services_acces">
-        <Link to="patients_db">
+        <Link to="hospital_db">
           <button className="patients">
-            <div className="services_patients_title">Patients database</div>
+            <div className="services_patients_title">Hospital database</div>
             <img
               className="patient_icon"
               src={db_icon}
@@ -26,9 +25,11 @@ function Services_patients() {
             ></img>
           </button>
         </Link>
-        <Link to="add_patient">
+        <Link to="add_hospital">
           <button className="hospitals">
-            <div className="services_hospitals_title">Adding a new patient</div>
+            <div className="services_hospitals_title">
+              Adding a new hospital
+            </div>
             <img
               className="hospital_icon"
               src={add_icon}
@@ -36,9 +37,9 @@ function Services_patients() {
             ></img>
           </button>
         </Link>
-        <Link to="interactive_map_patients">
+        <Link to="interactive_map_hospital">
           <button className="staff">
-            <div className="services_staff_title">Interactive patients map</div>
+            <div className="services_staff_title">Interactive hospital map</div>
             <img className="staff_icon" src={map_icon} alt="staff_icon"></img>
           </button>
         </Link>
@@ -49,4 +50,4 @@ function Services_patients() {
     </div>
   );
 }
-export default Services_patients;
+export default Services_hospitals;
